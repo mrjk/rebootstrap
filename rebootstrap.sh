49ca0e61c57a78ae823c6e80f6ae91f49e1b89a9
root <root@host>
2021-10-27 20:42:09 -0400

    Wip

--- rebootstrap.sh
+++ rebootstrap.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#  51 Fresh install
+#  52 udev                                       16G     0   16G   0% /dev
+#  53 tmpfs                                     3.2G  1.4M  3.2G   1% /run
+#  54 /dev/mapper/vg_fast-debian_green_root     4.9G  717M  3.9G  16% /
+#  55 tmpfs                                      16G     0   16G   0% /dev/shm
+#  56 tmpfs                                     5.0M     0  5.0M   0% /run/lock
+#  57 /dev/mapper/vg_fast-debian_green_var      976M  151M  759M  17% /var
+#  58 /dev/sda3                                 3.7G   64M  3.4G   2% /boot
+#  59 /dev/mapper/vg_fast-debian_green_var_lib  2.0G   61M  1.8G   4% /var/lib
+#  60 /dev/mapper/vg_fast-share                 9.8G   37M  9.3G   1% /share
+#  61 /dev/mapper/vg_fast-debian_green_var_log  2.0G   15M  1.8G   1% /var/log
+#  62 /dev/mapper/vg_fast-debian_green_tmp      2.0G  6.1M  1.8G   1% /tmp
+
+
 set -eu
 # Documentation
 # =================
@@ -18,7 +32,7 @@ CONF_FS=ext4
 CONF_DISK_PREFIX=debian_blue_
 CONF_GRUB_DEVICE=/dev/sda
 # CONF_GRUB_DEVICE=/dev/sdb
-
+CONF_DATE_CODE=$(date +%d/%m/%Y)
 
 ######   DEPRECATED  # Libraries
 ######   DEPRECATED  # =================
@@ -60,80 +74,80 @@ os_install_grml ()
 #     column -t > $FS_CHROOT/etc/fstab
 # }
 
-os_configure_old ()
-{
-
-  # Sanity check
-  if [[ -z "${FS_CHROOT:-}" ]]; then
-    echo "Big error here !"
-    return 2
-  fi
-
-  set -x
-  mkdir -p $FS_CHROOT/etc/
-
-  echo "nameserver 8.8.8.8" > $FS_CHROOT/etc/resolv.conf
-
-  echo $HOSTNAME > $FS_CHROOT/etc/hostname
-  echo -e "127.0.1.1\t$HOSTNAME" > $FS_CHROOT/etc/hosts
-
-  local def_interface=$(ip route | sed -n '1{s/.* dev //;s/ .*//;p}')
-  mkdir -p $FS_CHROOT/etc/network/interfaces.d/
-  echo -e "auto $def_interface\niface $def_interface inet dhcp\n" > $FS_CHROOT/etc/network/interfaces.d/$def_interface
-
-  # Chrooted version
-  # cat /proc/self/mounts | grep '^/dev/' > /etc/fstab
-
-  # External version
-
-
-#    mkdir -p $FS_CHROOT/etc/apt/
-#    cat > $FS_CHROOT/etc/apt/sources.list <<EOF
-#  deb http://ftp.ca.debian.org/debian/ $CONF_OS main contrib non-free
-#  deb-src http://ftp.ca.debian.org/debian/ $CONF_OS main contrib non-free
-#  
-#  deb http://security.debian.org/debian-security $CONF_OS/updates main
-#  deb-src http://security.debian.org/debian-security $CONF_OS/updates main
-#  
-#  # $CONF_OS-updates, previously known as 'volatile'
-#  deb http://ftp.ca.debian.org/debian/ $CONF_OS-updates main
-#  deb-src http://ftp.ca.debian.org/debian/ $CONF_OS-updates main
-#  
-#  # This system was installed using small removable media
-#  # (e.g. netinst, live or single CD). The matching "deb cdrom"
-#  # entries were disabled at the end of the installation process.
-#  # For information about how to configure apt package sources,
-#  # see the sources.list(5) manual.
-#  
-#  deb http://deb.debian.org/debian $CONF_OS-backports main
-#  EOF
-#  
-#  set +x
-#  echo "Configured into root: $FS_CHROOT"
-
-  return
-
-
-
-  # v1 wrap_exec grml-debootstrap --release bullseye  --target /mnt/debian_red --grub /dev/sda --defaultinterfaces --contrib --non-free --sshcopyid --debopt --cache-dir=/tmp/debootstrap/  -v --nodebootstrap
-  wrap_exec grml-debootstrap \
-    --debopt --cache-dir=/tmp/debootstrap/ \
-    --target /mnt/debian_red \
-    --release bullseye \
-    --grub /dev/sda \
-    --defaultinterfaces \
-    --contrib \
-    --non-free \
-    -v \
-    --force \
-    --sshcopyid \
-    --packages <(echo "$DEFAULT_PACKAGES") \
-    --password "qwerty78"
-
-    # --nodebootstrap \
-
-  # --sshcopyauth
-}
+######  os_configure_old ()
+######  {
+######  
+######    # Sanity check
+######    if [[ -z "${FS_CHROOT:-}" ]]; then
+######      echo "Big error here !"
+######      return 2
+######    fi
+######  
+######    set -x
+######    mkdir -p $FS_CHROOT/etc/
+######  
+######    echo "nameserver 8.8.8.8" > $FS_CHROOT/etc/resolv.conf
+######  
+######    echo $HOSTNAME > $FS_CHROOT/etc/hostname
+######    echo -e "127.0.1.1\t$HOSTNAME" > $FS_CHROOT/etc/hosts
+######  
+######    local def_interface=$(ip route | sed -n '1{s/.* dev //;s/ .*//;p}')
+######    mkdir -p $FS_CHROOT/etc/network/interfaces.d/
+######    echo -e "auto $def_interface\niface $def_interface inet dhcp\n" > $FS_CHROOT/etc/network/interfaces.d/$def_interface
+######  
+######    # Chrooted version
+######    # cat /proc/self/mounts | grep '^/dev/' > /etc/fstab
+######  
+######    # External version
+######  
+######  
+######  #    mkdir -p $FS_CHROOT/etc/apt/
+######  #    cat > $FS_CHROOT/etc/apt/sources.list <<EOF
+######  #  deb http://ftp.ca.debian.org/debian/ $CONF_OS main contrib non-free
+######  #  deb-src http://ftp.ca.debian.org/debian/ $CONF_OS main contrib non-free
+######  #  
+######  #  deb http://security.debian.org/debian-security $CONF_OS/updates main
+######  #  deb-src http://security.debian.org/debian-security $CONF_OS/updates main
+######  #  
+######  #  # $CONF_OS-updates, previously known as 'volatile'
+######  #  deb http://ftp.ca.debian.org/debian/ $CONF_OS-updates main
+######  #  deb-src http://ftp.ca.debian.org/debian/ $CONF_OS-updates main
+######  #  
+######  #  # This system was installed using small removable media
+######  #  # (e.g. netinst, live or single CD). The matching "deb cdrom"
+######  #  # entries were disabled at the end of the installation process.
+######  #  # For information about how to configure apt package sources,
+######  #  # see the sources.list(5) manual.
+######  #  
+######  #  deb http://deb.debian.org/debian $CONF_OS-backports main
+######  #  EOF
+######  #  
+######  #  set +x
+######  #  echo "Configured into root: $FS_CHROOT"
+######  
+######    return
+######  
+######  
+######  
+######    # v1 wrap_exec grml-debootstrap --release bullseye  --target /mnt/debian_red --grub /dev/sda --defaultinterfaces --contrib --non-free --sshcopyid --debopt --cache-dir=/tmp/debootstrap/  -v --nodebootstrap
+######    wrap_exec grml-debootstrap \
+######      --debopt --cache-dir=/tmp/debootstrap/ \
+######      --target /mnt/debian_red \
+######      --release bullseye \
+######      --grub /dev/sda \
+######      --defaultinterfaces \
+######      --contrib \
+######      --non-free \
+######      -v \
+######      --force \
+######      --sshcopyid \
+######      --packages <(echo "$DEFAULT_PACKAGES") \
+######      --password "qwerty78"
+######  
+######      # --nodebootstrap \
+######  
+######    # --sshcopyauth
+######  }
 
 
 
@@ -538,6 +552,7 @@ api_os_preconfigure ()
   fi
 
   # Configure apt
+  # TODO: Add other repos !
   outfile=$FS_CHROOT/etc/apt/apt.conf.d/02nosuggest
   _log INFO "Configure $outfile"
   if ! ${RESTRAP_DRY:-false}; then
@@ -546,6 +561,7 @@ APT::Install-Recommends "1";
 APT::Install-Suggests "0";
 EOF
   fi
+
 
   # Configure locale
   infile=/etc/default/locale
@@ -566,19 +582,35 @@ EOF
   fi
   api_os_chroot locale-gen
 
-  # Import resolvers
-  infile=/etc/systemd/resolved.conf.d
+  # Import resolvers (systemd-resolverd is the choice)
   _log INFO "Import systemd resolved config"
+  infile=/etc/resolv.conf
+  if [[ -e "$infile" ]]; then
+    _exec cp --preserve=links $infile $FS_CHROOT$infile
+  else
+    _exec ln -s $infile $FS_CHROOT$infile
+  fi
+  infile=/etc/systemd/resolved.conf.d
   if [[ -d "$infile/" ]]; then
     _exec cp -R $infile/. $FS_CHROOT$infile
+  else
+    if ! ${RESTRAP_DRY:-false}; then
+      _exec mkdir -p $FS_CHROOT$infile
+      cat > $FS_CHROOT$infile/default.conf << EOF
+[Resolve]
+DNS=127.0.0.53
+EOF
+    fi
   fi
+  api_os_chroot systemctl enable systemd-resolved
 
-  # Import networkd
+  # Import networkd (use systemd if enabled)
   infile=/etc/systemd/network
   _log INFO "Import systemd resolved config"
   if [[ -d "$infile/" ]]; then
     _exec cp -R $infile/. $FS_CHROOT$infile
-    api_os_chroot systemctl enable systemd-networkd
+    api_os_chroot systemctl disable networking.service
+    api_os_chroot systemctl enable systemd-networkd.service
   fi
 
 
@@ -599,16 +631,37 @@ EOF
   _log INFO "Import root directory (partial import)"
   _exec rsync -av \
 		--include={.config,.profile,.bashrc,.vimrc,.ssh} \
-		--exclude='.*' \
+		--exclude='/.*' \
 		/root/ $FS_CHROOT/root
 
+  # Import grub config
+  infile=/etc/default/grub
+  if [[ -f "$infile" ]]; then
+    _log INFO "Import $infile"
+    _exec cp "$infile" "$FS_CHROOT$infile"
+  else
+    if ! ${RESTRAP_DRY:-false}; then
+      cat > "$FS_CHROOT$infile" <<EOF
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=5
+GRUB_DISTRIBUTOR=Debian-${DIST_COLOR^}
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX=""
+GRUB_PRELOAD_MODULES="diskfilter lvm mdraid1x"
+EOF
+    fi
+  fi
+
   # Save the date
-  outfile=$FS_CHROOT/root/INFO
+  outfile=$FS_CHROOT/etc/install-release
   _log INFO "Configure $outfile"
   if ! ${RESTRAP_DRY:-false}; then
-    echo "INSTALL_DATE=$(date --iso-8601=seconds)" > $outfile
+      cat > "$outfile" <<EOF
+INSTALL_DATE="$(date --iso-8601=seconds)"
+INSTALL_VARIANT="${DIST_COLOR}"
+INSTALL_PRETTY_VARIANT="${DIST_COLOR^}"
+EOF
   fi
-  
 
 }
 
@@ -626,7 +679,7 @@ api_os__gen_fstab ()
   # Keep existing mounts
   echo "# Imported mounts"
   cat /etc/fstab | \
-    grep -E -v "#.*|/ |/boot |/tmp |/usr |/bin |/sbin |/var |/var/log |/var/lib| swap " | \
+    grep -E -v "#.*|/ |/boot |/tmp |/usr |/bin |/sbin |/var |/var/log |/var/lib " | \
     sed '/^$/d' | column -t
 }
 
@@ -642,6 +695,8 @@ api_os_bootloader ()
       # TOFIX: Enable this to next boot on this distro
       _log INFO "Next boot: Target OS /!\\"
       api_os_chroot grub-install $disk
+    else
+      api_os_chroot grub-install --no-bootsector $disk
     fi
   fi
   api_os_chroot update-grub
@@ -649,11 +704,17 @@ api_os_bootloader ()
   # Exec local update to detect this new OS
   _exec grub-mkdevicemap
   if $autoboot ; then
-    _log INFO "Next boot: Current OS"
     _exec grub-install $disk
+  else
+    _log INFO "Next boot: Current OS"
+    _exec grub-install --no-bootsector $disk
   fi
   _exec update-grub
-  _log INFO "User import finished"
+  if $autoboot ; then
+    _log INFO "User import finished, OS will reboot on TARGET OS."
+  else
+    _log INFO "User import finished, system will reboot on the current OS."
+  fi
 }
 
 #api_os_bootloader_from_host ()
@@ -839,9 +900,89 @@ cli__help ()
     | xargs -n2 -d'\n' | column -t -s ',' 
 }
 
+
+
+
+################ DEVEL
+
+init_config ()
+{
+  local current_os_disk
+  current_os_disk=${1:-$(mount | sed  -n '/ on \/ type/s@ on / .*@@p')}
+  OS_MAP=$(grep -v '^#' <<< "$OS_MAP" | grep -v "^$" )
+  OS_MAP_TMP=$(echo -e "$OS_MAP\n$OS_MAP" )
+
+#  echo "${current_os_disk}" | grep "/${VG_NAME}-${DISK_NAME_PREFIX}_[a-zA-Z0-9]*_root"
+#  echo "${current_os_disk}" | grep "/${VG_NAME}-${DISK_NAME_PREFIX}_[a-zA-Z0-9]*_root"
+  name=$(sed -E -n "s@/.*/${VG_NAME}-${DISK_NAME_PREFIX}_([a-zA-Z0-9]*)_root@\\1@p" <<< "$current_os_disk" )
+
+  echo "NAME: $name"
+  if [[ -z "$name" ]]; then
+    _log ERROR "Can't auto-detect current configuration name, please use -n option."
+    exit 1
+  fi
+
+  # Verify config
+  config_match=$( grep "^$name" <<< "$OS_MAP") || {
+    _log ERROR "Cant' find config '$name' in config file"
+    list=$(grep -o '^[a-zA-Z0-9][a-zA-Z0-9]*' <<< "$OS_MAP" | tr '\n' ',' | sed 's/,$//')
+    _log HINT "Available configs: $list"
+    return 1
+  }
+
+set -x
+  : "$OS_MAP"
+  next_config=$( grep -A 1 "^$name" <<< "$OS_MAP_TMP" | sed -n '2p' )
+#  last_option=$(tail -n 1 <<< "$OS_MAP" )
+  #if [[ "$next_config" == "$last_option" ]]; then
+  #  # take the first one then
+  #  next_config=$(head -n 1 <<< "$OS_MAP")
+  #fi
+  set +x
+
+
+  # Load config
+  #while  read -r name grub_disk os prefix _ ; do
+  #  :
+  echo "NEXT_CONFIG=$next_config"
+  read -r name os grub_disk _  <<< "$next_config"
+  echo "Weee: ${DISK_NAME_PREFIX}_$name boot on $grub_disk"
+
+  # echo "$PART_MAP2" | column -t
+
+
+
+
+
+
+
+}
+
+loop_over_cfg_v3 ()
+{
+  local args=$@
+  local config="$OS_MAP"
+
+  # Loop over entries
+  while  read -r name grub_disk os prefix _ ; do
+
+    [[ ! -z "$name" ]] || continue
+    [[ ! "$name" =~ ^' '*'#' ]] || continue
+
+    # $cmd $args
+    for cmd in $args; do
+      $cmd
+    done
+
+  done <<<"$config"
+}
+
+
 cli__devel ()
 {
-  api_os_bootloader
+  . /root/prj/rebootstrap/configs/new_conf.sh
+  init_config $@
+  #loop_over_cfg_v3
 }
 
 
