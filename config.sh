e97c9617f6222f28d9820d5aaa2ab091cda2dcca
root <root@host>
2021-10-29 03:43:42 -0400

    Add: Sample config

---
+++ config.sh
@@ -0,0 +1,24 @@
+# Rebootstrap configuration example
+
+DEFAULT_VG=vg_fast
+DEFAULT_OS=bullseye
+DEFAULT_GRUB=mbr
+DEFAULT_PREFIX=debian
+DEFAULT_BOOT=current
+
+OS_MAP="
+#name   os        grub            vg
+green   bullseye  mbr,/dev/sda2    vg_fast
+blue    bullseye  mbr,/dev/sda3    vg_fast
+"
+
+PART_MAP="
+#Mount    device     format  size  mount_opt
+/         -          ext4    5G    -
+/boot     -          ext4    -     -
+/var      -          ext4    1G    -
+/var/lib  -          ext4    2G    -
+/var/log  -          ext4    2G    -
+/tmp/     -          ext4    2G
+swap      -          swap    4G
+"
